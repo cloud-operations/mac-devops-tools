632d638792116bcb5c47a923ac2c2ffd3f38818d
Sunil <sunil@Sunils-MacBook-Air.local>
2022-11-25 12:11:23 -0800

    tools script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#xcode-select --install
+
+brew install git
+
+#curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+brew install --cask iterm2
+
+brew cask install docker
+
+brew install java
+sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+
+
+brew install --cask visual-studio-code
+
+brew install python
+
+brew install gradle
+
+
+
